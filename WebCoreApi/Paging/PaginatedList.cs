28928919e91891cf1a3d0bb76aeae8a07c1d50e3
paulmcpg <84036679+paulmcpg@users.noreply.github.com>
2022-02-28 16:14:25 -0500

    added

--- WebCoreApi/Paging/PaginatedList.cs
+++ WebCoreApi/Paging/PaginatedList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace myNamespace.Paging
+namespace xAI.Paging
 {
     public class PaginatedList<T> : List<T>
     {
--- WebCoreApi/Program.cs
+++ WebCoreApi/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace myNamespace
+namespace xAI
 {
     public class Program
     {
