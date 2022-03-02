95cdb2d17bb4020ae37097a5c6833892b922d224
paulmcpg <84036679+paulmcpg@users.noreply.github.com>
2022-03-02 12:39:50 -0500

    moddeddbinit

---
+++ WebCoreApi/Data/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using xAI.Models;
+
+namespace xAI.Data
+{
+    public class xAIContext : DbContext
+    {
+
+        public xAIContext() { }
+
+        public xAIContext(DbContextOptions<xAIContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=xaidemo;Trusted_Connection=True;MultipleActiveResultSets=True");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
